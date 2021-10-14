85accf28838f093f2309bc91e87b32a95369d8d9
Malcolm Scruggs <malcolm.scruggs@appfolio.com>
2021-10-14 16:17:15 -0700

    base generate graphql types

---
+++ app/graphql/types/area_type.rb
@@ -0,0 +1,11 @@
+module Types
+  class AreaType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :position, Integer, null: true
+    field :tombstone, Boolean, null: false
+    field :inspection_id, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
---
+++ app/graphql/types/item_type.rb
@@ -0,0 +1,12 @@
+module Types
+  class ItemType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :note, String, null: true
+    field :flagged, Boolean, null: false
+    field :tombstone, Boolean, null: false
+    field :area_id, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
