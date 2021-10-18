585117d68f6e52894a636e9d0730c948b8425062
Malcolm Scruggs <malcolm.scruggs@appfolio.com>
2021-10-18 14:23:46 -0700

    Sloppy implementation of all create inspections

--- app/graphql/mutations/create_inspection.rb
+++ app/graphql/mutations/create_inspection.rb
@@ -8,7 +8,17 @@ module Mutations
     argument :inspection, Types::InspectionAttributes, required: true
 
     def resolve(inspection:)
-      record = Inspection.create(**inspection)
+      inspection_hash = inspection.to_hash
+      areas = inspection_hash[:areas]
+      record = Inspection.create(inspection_hash.except(:areas))
+      areas&.each do |area|
+        area_args = area.to_hash.merge(inspection: record)
+        items = area_args.delete(:items)
+        area_record = Area.create(area_args)
+        items&.each do |item|
+          Item.create(item.to_hash.merge(area: area_record))
+        end
+      end
 
       if record.save
         { success: true, inspection: record, errors: []}
--- app/graphql/types/item_attributes.rb
+++ app/graphql/types/item_attributes.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 module Types
-  class AreaAttributes < Types::BaseInputObject
-    description 'Attributes for creating an area'
+  class ItemAttributes < Types::BaseInputObject
+    description 'Attributes for creating an item'
     # TODO: can this be re-used for updates and if so how to mark everything optional
     argument :id, Integer, required: false
     argument :name, String, required: true
+    argument :note, String, required: false
+    argument :flagged, Boolean, required: false
     argument :position, Integer, required: false
     argument :tombstone, Boolean, required: false
 
-    argument :items, [Types::ItemAttributes], required: false
   end
 end
