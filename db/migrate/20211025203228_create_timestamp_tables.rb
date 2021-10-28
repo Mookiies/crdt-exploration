9adac6dc9aeffabd6185496e94ecfde6353540d3
Malcolm Scruggs <malcolm.scruggs@appfolio.com>
2021-10-28 09:31:41 -0700

    Modify timestamp migration to contain unique constraint

--- db/migrate/20211025203228_create_timestamp_tables.rb
+++ db/migrate/20211025203228_create_timestamp_tables.rb
@@ -3,14 +3,14 @@ class CreateTimestampTables < ActiveRecord::Migration[6.1]
     create_table :inspections_timestamps do |t|
       t.string :name_ts
 
-      t.belongs_to :inspection, foreign_key: true, null: false
+      t.belongs_to :inspection, foreign_key: true, null: false, index: { unique: true }
     end
 
     create_table :areas_timestamps do |t|
       t.string :name_ts
       t.string :position_ts
 
-      t.belongs_to :area, foreign_key: true, null: false
+      t.belongs_to :area, foreign_key: true, null: false, index: { unique: true }
     end
 
     create_table :items_timestamps do |t|
@@ -18,7 +18,7 @@ class CreateTimestampTables < ActiveRecord::Migration[6.1]
       t.string :note_ts
       t.string :flagged_ts
 
-      t.belongs_to :item, foreign_key: true, null: false
+      t.belongs_to :item, foreign_key: true, null: false, index: { unique: true }
     end
   end
 end
